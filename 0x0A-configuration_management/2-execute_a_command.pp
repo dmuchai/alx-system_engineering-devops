a0d2616a49d876f85f1bd295053cab831c9f80c1
Your Name <dmmuchai@gmail.com>
2025-01-10 07:59:22 -0800

    Task 2. Execute a command

--- 0x0A-configuration_management/2-execute_a_command.pp
+++ 0x0A-configuration_management/2-execute_a_command.pp
@@ -1,5 +1,5 @@
 # This Puppet manifest kills a process named 'killmenow' using the pkill command.
-exec { 'kill_process_killmenow':
+exec { 'killmenow':
   command   => 'pkill killmenow',
   path      => ['/bin', '/usr/bin', '/sbin', '/usr/sbin'],
   onlyif    => 'pgrep killmenow',
