ca7c351c577244407a3a6a98b5a549cbe665f7cc
Your Name <dmmuchai@gmail.com>
2025-01-11 06:35:56 -0800

    Task 1. Install a package

--- 0x0A-configuration_management/1-install_a_package.pp
+++ 0x0A-configuration_management/1-install_a_package.pp
@@ -1,5 +1,11 @@
-# Puppet installs the Flask package (version 2.1.0) using pip3.
+# This Puppet manifest installs Flask 2.1.0 and Werkzeug 2.1.1 using pip.
 package { 'flask':
   ensure   => '2.1.0',
-  provider => 'pip3',
+  provider => 'pip',
+}
+
+package { 'werkzeug':
+  ensure   => '2.1.1',
+  provider => 'pip',
+  require  => Package['flask'], # Ensure Werkzeug is installed after Flask
 }
