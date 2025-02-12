3b3a0c8346553e1727161626ae0a042d3679227d
Dennis Muniu Muchai <dmmuchai@gmail.com>
2025-02-12 02:35:19 -0800

    Task 0. Gather data from an API

---
+++ 0x15-api/0-gather_data_from_an_API.py
@@ -0,0 +1,41 @@
+#!/usr/bin/python3
+"""
+This script fetches and displays TODO list progress of a given employee
+using a REST API.
+"""
+
+import requests
+import sys
+
+
+if __name__ == "__main__":
+    if len(sys.argv) != 2 or not sys.argv[1].isdigit():
+        print("Usage: {} <employee_id>".format(sys.argv[0]))
+        sys.exit(1)
+
+    employee_id = int(sys.argv[1])
+    base_url = "https://jsonplaceholder.typicode.com"
+
+    # Fetch user details
+    user_url = "{}/users/{}".format(base_url, employee_id)
+    user_response = requests.get(user_url)
+    user_data = user_response.json()
+
+    # Fetch TODO list
+    todos_url = "{}/todos?userId={}".format(base_url, employee_id)
+    todos_response = requests.get(todos_url)
+    todos_data = todos_response.json()
+
+    if not user_data or not todos_data:
+        print("No data found for Employee ID:", employee_id)
+        sys.exit(1)
+
+    employee_name = user_data.get("name")
+    total_tasks = len(todos_data)
+    done_tasks = [task.get("title") for task in todos_data if task.get("completed")]
+
+    print("Employee {} is done with tasks({}/{}):".format(
+        employee_name, len(done_tasks), total_tasks))
+
+    for task in done_tasks:
+        print("\t {}".format(task))
