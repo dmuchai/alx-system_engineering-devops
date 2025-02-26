814870842cd3a4a692aa740adfd1b43e8cba7d41
dmuchai <dmmuchai@gmail.com>
2025-02-26 05:15:03 +0300

    Task 0. How many subs?

---
+++ 0x16-api_advanced/0-subs.py
@@ -0,0 +1,16 @@
+import requests
+
+def number_of_subscribers(subreddit):
+    """
+    Queries the Reddit API and returns the number of subscribers
+    for a given subreddit. Returns 0 if the subreddit is invalid.
+    """
+    url = f"https://www.reddit.com/r/{subreddit}/about.json"
+    headers = {'User-Agent': 'ALX-Advanced-API-Client/1.0'}
+    response = requests.get(url, headers=headers, allow_redirects=False)
+    
+    if response.status_code != 200:
+        return 0
+    
+    data = response.json()
+    return data.get('data', {}).get('subscribers', 0)
