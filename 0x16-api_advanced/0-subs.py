85ea5cf729099e8ba120b9b81867acaa8b35a5f5
dmuchai <dmmuchai@gmail.com>
2025-02-26 05:22:12 +0300

    Task 0. How many subs updates

--- 0x16-api_advanced/0-subs.py
+++ 0x16-api_advanced/0-subs.py
@@ -1,5 +1,6 @@
 import requests
 
+
 def number_of_subscribers(subreddit):
     """
     Queries the Reddit API and returns the number of subscribers
@@ -8,9 +9,9 @@ def number_of_subscribers(subreddit):
     url = f"https://www.reddit.com/r/{subreddit}/about.json"
     headers = {'User-Agent': 'ALX-Advanced-API-Client/1.0'}
     response = requests.get(url, headers=headers, allow_redirects=False)
-    
+
     if response.status_code != 200:
         return 0
-    
+
     data = response.json()
     return data.get('data', {}).get('subscribers', 0)
