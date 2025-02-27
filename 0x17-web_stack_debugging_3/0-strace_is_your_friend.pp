a9b1d66029dbf0300b42626cc7313ac16b445485
dmmuchai <dmmuchai@gmail.com>
2025-02-27 16:26:26 +0000

    Task 0. Debugging with Strace

--- 0x17-web_stack_debugging_3/0-strace_is_your_friend.pp
+++ 0x17-web_stack_debugging_3/0-strace_is_your_friend.pp
@@ -1,5 +1,6 @@
 # Fixes a faulty WordPress site by correcting the .phpp to .php in wp-settings.php
 exec { 'fix-wordpress':
-  command => 'sed -i s/class-wp-locale.phpp/class-wp-locale.php/ /var/www/html/wp-settings.php',
+  command => 'bash -c "sed -i s/class-wp-locale.phpp/class-wp-locale.php/ \
+/var/www/html/wp-settings.php; service apache2 restart"',
   path    => '/usr/bin:/usr/sbin:/bin'
 }
