4c72921005e8cc6af9ea69475c610a78ece594c2
dmmuchai <dmmuchai@gmail.com>
2025-02-28 12:52:29 +0000

    Task updates

--- 0x17-web_stack_debugging_3/0-strace_is_your_friend.pp
+++ 0x17-web_stack_debugging_3/0-strace_is_your_friend.pp
@@ -2,5 +2,5 @@
 
 exec { 'fix-wordpress':
   command => 'sed -i s/phpp/php/g /var/www/html/wp-settings.php',
-  path    => ['/bin', '/usr/bin/', '/usr/loca/bin/'],
+  path    => ['/usr/local/sbin:/usr/local/bin:/usr/sbin:/usr/bin:/sbin:/bin'],
 }
